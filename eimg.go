963c3f825dde5809143b96139daf011ca2436202
xplshn <musicslave66@murena.io>
2024-08-17 11:28:20 -0300

    Pre-dither images that aren't 256 colors when using sixel. Add flag to be able to use a specific encoding. Replace --resize-* with just --resize wxh

--- eimg.go
+++ eimg.go
@@ -1,3 +1,4 @@
+// eimg.go - Converts your images into encodings that your terminal&framebuffer can/may understand
 package main
 
 import (
@@ -5,34 +6,39 @@ import (
 	"flag"
 	"fmt"
 	"github.com/BourgeoisBear/rasterm"
+	"github.com/ericpauley/go-quantize/quantize"
 	"github.com/jiro4989/textimg/v3/config"
+	"github.com/makeworld-the-better-one/dither/v2"
 	"github.com/xplshn/a-utils/pkg/ccmd"
 	_ "golang.org/x/image/bmp"
 	_ "golang.org/x/image/tiff"
 	_ "golang.org/x/image/vector"
 	_ "golang.org/x/image/webp"
 	"image"
+	"image/color"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	inputFilePath := flag.String("input-file", "", "Input image file path")
-	resizeWidth := flag.Int("resize-width", 0, "Resize width")
-	resizeHeight := flag.Int("resize-height", 0, "Resize height")
+	resizeFlag := flag.String("resize", "", "Resize dimensions (e.g., 800x600)")
 	scaleFactor := flag.Float64("scale-factor", 1.0, "Scale factor")
 	posX := flag.Int("pos-x", 0, "X position on framebuffer")
 	posY := flag.Int("pos-y", 0, "Y position on framebuffer")
 	noBounds := flag.Bool("no-bounds", false, "Disable safety feature to keep image in-bounds")
+	useEncoding := flag.String("use-encoding", "", "Force specific encoding (kitty, iterm, sixel)")
 
 	cmdInfo := &ccmd.CmdInfo{
 		Name:        "eimg",
 		Repository:  "https://github.com/xplshn/eimg.git",
 		Authors:     []string{"xplshn"},
-		Synopsis:    "[--input-file [FILE]] <|--resize-{width,height}|--scale-factor|--pos-{x,y}|--no-bounds|>",
+		Synopsis:    "[--input-file [FILE]] <|--resize|--scale-factor|--pos-{x,y}|--no-bounds|--use-encoding|>",
 		Description: "Displays images in the terminal using terminal encodings",
 	}
 
@@ -73,104 +79,148 @@ func main() {
 		os.Exit(1)
 	}
 
+	resizeWidth, resizeHeight := 0, 0
+	if *resizeFlag != "" {
+		parts := strings.Split(*resizeFlag, "x")
+		if len(parts) != 2 {
+			fmt.Print(helpPage)
+			fmt.Fprintln(os.Stderr, "Invalid resize format. Use WIDTHxHEIGHT (e.g., 800x600)")
+			os.Exit(1)
+		}
+		var err error
+		resizeWidth, err = strconv.Atoi(parts[0])
+		if err != nil {
+			fmt.Print(helpPage)
+			fmt.Fprintln(os.Stderr, "Invalid width value")
+			os.Exit(1)
+		}
+		resizeHeight, err = strconv.Atoi(parts[1])
+		if err != nil {
+			fmt.Print(helpPage)
+			fmt.Fprintln(os.Stderr, "Invalid height value")
+			os.Exit(1)
+		}
+	}
+
 	cfg := config.Config{
-		ResizeWidth:   *resizeWidth,
-		ResizeHeight:  *resizeHeight,
+		ResizeWidth:   resizeWidth,
+		ResizeHeight:  resizeHeight,
 		FileExtension: fileExtension,
 		Writer:        os.Stdout, // Set the writer to stdout for terminal display
 	}
 
 	// INLINE FUNCTIONS
-	displayImage := func(cfg *config.Config, inputFilePath string, posX, posY int, scaleFactor float64, noBounds bool) error {
-		encodeAndDisplayImage := func(img image.Image, cfg *config.Config) error {
-			if rasterm.IsKittyCapable() {
+	convertToPaletted := func(img image.Image) *image.Paletted {
+		if palettedImg, ok := img.(*image.Paletted); ok && len(palettedImg.Palette) == 256 {
+			// The image is already paletted with a depth of 256, no need to convert.
+			return palettedImg
+		}
+
+		// Create a palette with 256 colors
+		q := quantize.MedianCutQuantizer{}
+		palette := q.Quantize(make([]color.Color, 0, 256), img)
+
+		d := dither.NewDitherer(palette)
+		d.Matrix = dither.Stucki
+		palettedImg := d.DitherPaletted(img)
+
+		return palettedImg
+	}
+	resizeImage := func(img image.Image, width, height int) image.Image {
+		if width == 0 && height == 0 {
+			return img
+		}
+
+		bounds := img.Bounds()
+		imgWidth := bounds.Dx()
+		imgHeight := bounds.Dy()
+
+		if width == 0 {
+			width = int(float64(imgWidth) * (float64(height) / float64(imgHeight)))
+		} else if height == 0 {
+			height = int(float64(imgHeight) * (float64(width) / float64(imgWidth)))
+		}
+
+		resizedImg := image.NewRGBA(image.Rect(0, 0, width, height))
+		for y := 0; y < height; y++ {
+			for x := 0; x < width; x++ {
+				srcX := x * imgWidth / width
+				srcY := y * imgHeight / height
+				resizedImg.Set(x, y, img.At(srcX, srcY))
+			}
+		}
+
+		return resizedImg
+	}
+	scaleImage := func(img image.Image, scaleFactor float64) image.Image {
+		if scaleFactor == 1.0 {
+			return img
+		}
+
+		bounds := img.Bounds()
+		imgWidth := bounds.Dx()
+		imgHeight := bounds.Dy()
+
+		scaledWidth := int(float64(imgWidth) * scaleFactor)
+		scaledHeight := int(float64(imgHeight) * scaleFactor)
+
+		scaledImg := image.NewRGBA(image.Rect(0, 0, scaledWidth, scaledHeight))
+		for y := 0; y < scaledHeight; y++ {
+			for x := 0; x < scaledWidth; x++ {
+				srcX := int(float64(x) / scaleFactor)
+				srcY := int(float64(y) / scaleFactor)
+				scaledImg.Set(x, y, img.At(srcX, srcY))
+			}
+		}
+
+		return scaledImg
+	}
+	ensureInBounds := func(img image.Image, maxWidth, maxHeight int) image.Image {
+		bounds := img.Bounds()
+		imgWidth := bounds.Dx()
+		imgHeight := bounds.Dy()
+
+		if imgWidth <= maxWidth && imgHeight <= maxHeight {
+			return img
+		}
+
+		newWidth := imgWidth
+		newHeight := imgHeight
+
+		if imgWidth > maxWidth {
+			newWidth = maxWidth
+			newHeight = int(float64(imgHeight) * (float64(maxWidth) / float64(imgWidth)))
+		}
+
+		if newHeight > maxHeight {
+			newHeight = maxHeight
+			newWidth = int(float64(imgWidth) * (float64(maxHeight) / float64(imgHeight)))
+		}
+
+		return resizeImage(img, newWidth, newHeight)
+	}
+	displayImage := func(cfg *config.Config, inputFilePath string, posX, posY int, scaleFactor float64, noBounds bool, useEncoding string) error {
+		encodeAndDisplayImage := func(img image.Image, cfg *config.Config, useEncoding string) error {
+			switch useEncoding {
+			case "kitty":
 				return rasterm.KittyWriteImage(cfg.Writer, img, rasterm.KittyImgOpts{})
-			} else if rasterm.IsItermCapable() {
+			case "iterm":
 				return rasterm.ItermWriteImage(cfg.Writer, img)
-			} else if isSixelCapable, err := rasterm.IsSixelCapable(); err == nil && isSixelCapable {
-				palettedImg := image.NewPaletted(img.Bounds(), nil)
-				for y := 0; y < img.Bounds().Dy(); y++ {
-					for x := 0; x < img.Bounds().Dx(); x++ {
-						palettedImg.Set(x, y, img.At(x, y))
-					}
-				}
+			case "sixel":
+				palettedImg := convertToPaletted(img)
 				return rasterm.SixelWriteImage(cfg.Writer, palettedImg)
-			}
-
-			return fmt.Errorf("terminal does not support any known image protocol")
-		}
-		resizeImage := func(img image.Image, width, height int) image.Image {
-			if width == 0 && height == 0 {
-				return img
-			}
-
-			bounds := img.Bounds()
-			imgWidth := bounds.Dx()
-			imgHeight := bounds.Dy()
-
-			if width == 0 {
-				width = int(float64(imgWidth) * (float64(height) / float64(imgHeight)))
-			} else if height == 0 {
-				height = int(float64(imgHeight) * (float64(width) / float64(imgWidth)))
-			}
-
-			resizedImg := image.NewRGBA(image.Rect(0, 0, width, height))
-			for y := 0; y < height; y++ {
-				for x := 0; x < width; x++ {
-					srcX := x * imgWidth / width
-					srcY := y * imgHeight / height
-					resizedImg.Set(x, y, img.At(srcX, srcY))
+			default:
+				if rasterm.IsKittyCapable() {
+					return rasterm.KittyWriteImage(cfg.Writer, img, rasterm.KittyImgOpts{})
+				} else if rasterm.IsItermCapable() {
+					return rasterm.ItermWriteImage(cfg.Writer, img)
+				} else if isSixelCapable, err := rasterm.IsSixelCapable(); err == nil && isSixelCapable {
+					palettedImg := convertToPaletted(img)
+					return rasterm.SixelWriteImage(cfg.Writer, palettedImg)
 				}
+
+				return fmt.Errorf("terminal does not support any known image protocol")
 			}
-
-			return resizedImg
-		}
-		scaleImage := func(img image.Image, scaleFactor float64) image.Image {
-			if scaleFactor == 1.0 {
-				return img
-			}
-
-			bounds := img.Bounds()
-			imgWidth := bounds.Dx()
-			imgHeight := bounds.Dy()
-
-			scaledWidth := int(float64(imgWidth) * scaleFactor)
-			scaledHeight := int(float64(imgHeight) * scaleFactor)
-
-			scaledImg := image.NewRGBA(image.Rect(0, 0, scaledWidth, scaledHeight))
-			for y := 0; y < scaledHeight; y++ {
-				for x := 0; x < scaledWidth; x++ {
-					srcX := int(float64(x) / scaleFactor)
-					srcY := int(float64(y) / scaleFactor)
-					scaledImg.Set(x, y, img.At(srcX, srcY))
-				}
-			}
-
-			return scaledImg
-		}
-		ensureInBounds := func(img image.Image, maxWidth, maxHeight int) image.Image {
-			bounds := img.Bounds()
-			imgWidth := bounds.Dx()
-			imgHeight := bounds.Dy()
-
-			if imgWidth <= maxWidth && imgHeight <= maxHeight {
-				return img
-			}
-
-			newWidth := imgWidth
-			newHeight := imgHeight
-
-			if imgWidth > maxWidth {
-				newWidth = maxWidth
-				newHeight = int(float64(imgHeight) * (float64(maxWidth) / float64(imgWidth)))
-			}
-
-			if newHeight > maxHeight {
-				newHeight = maxHeight
-				newWidth = int(float64(imgWidth) * (float64(maxHeight) / float64(imgHeight)))
-			}
-
-			return resizeImage(img, newWidth, newHeight)
 		}
 		// E-OF-INLINE-FUNCTIONS
 
@@ -205,7 +255,7 @@ func main() {
 		}
 
 		// Encode and display the image for terminal display
-		if err := encodeAndDisplayImage(img, cfg); err != nil {
+		if err := encodeAndDisplayImage(img, cfg, useEncoding); err != nil {
 			// Fallback to framebuffer if no terminal protocol is found
 			// Hide the cursor
 			fmt.Print("\033[?25l")
@@ -219,7 +269,7 @@ func main() {
 	}
 	// E-OF-INLINE-FUNCTIONS
 
-	if err := displayImage(&cfg, *inputFilePath, *posX, *posY, *scaleFactor, *noBounds); err != nil {
+	if err := displayImage(&cfg, *inputFilePath, *posX, *posY, *scaleFactor, *noBounds, *useEncoding); err != nil {
 		fmt.Println("Error:", err)
 	}
 	println()
--- go.mod
+++ go.mod
@@ -4,15 +4,17 @@ go 1.22.5
 
 require (
 	github.com/BourgeoisBear/rasterm v1.1.1
+	github.com/ericpauley/go-quantize v0.0.0-20200331213906-ae555eb2afa4
 	github.com/jiro4989/textimg/v3 v3.1.10
+	github.com/makeworld-the-better-one/dither/v2 v2.4.0
 	github.com/orangecms/go-framebuffer v0.0.0-20200613202404-a0700d90c330
-	github.com/xplshn/a-utils v0.0.0-20240730224731-8b111c19faa1
-	golang.org/x/image v0.18.0
+	github.com/xplshn/a-utils v0.0.0-20240801030140-4819c954217d
+	golang.org/x/image v0.19.0
 )
 
 require (
 	github.com/kaey/framebuffer v0.0.0-20140402104929-7b385489a1ff // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/term v0.22.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/term v0.23.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 )
